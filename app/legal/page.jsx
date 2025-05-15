5ef25e95929e9be20d01b2bfa6952618d185c692
natalia4566 <140755966+natalia4566@users.noreply.github.com>
2025-05-15 15:43:11 -0500

    Update page.jsx

--- app/legal/page.jsx
+++ app/legal/page.jsx
@@ -7,49 +7,50 @@ const Legal = () => {
   const [activo, setActivo] = useState(null);
 
   const documentosPorAno = {
-    2020: [
-      { nombre: 'Denominacion de  domicilio.', url: '/Documentos/2020/1.-Denominacion-domicilio.pdf' },
-      { nombre: 'Descripcion actividad meritoria.', url: '/documentos/2020/2.-Descripcion-Actividad-Meritoria.pdf' },
-      { nombre: 'Certificado requisitos RL.', url: '/documentos/2020/3.Certificado-Requisitos-RL.pdf' },
-      { nombre: 'Certificación antecedentes judiciales RL.', url: '/documentos/2020/4.-Certificacion-Antecedentes-Judic-RL.pdf' },
-      { nombre: 'Nombre de los cargos cirectivos y de control.', url: '/documentos/2020/5.-Nombre-de-los-Cargos-Directivos-y-de-Control.pdf' },
-      { nombre: 'Identificación de fundadores.', url: '/documentos/2020/6.Identificacion-Fundadores.pdf' },
-      { nombre: 'Estados rinancieros 2020.', url: '/documentos/2020/Estados-Financieros-2020.pdf' }
-    ],
-    2021: [
-      { nombre: 'Denominacion de  domicilio.', url: '/Documentos/2021/1.Denominacion-Domicilio-e-Identificacion.pdf' },
-      { nombre: 'Descripcion actividad meritoria.', url: '/documentos/2021/2.-Descripcion-de-la-Actividad-Meritoria.pdf' },
-      { nombre: 'Certificado requisitos RL.', url: '/documentos/2021/3.-Certificado-Requisitos-RL.pdf' },
-      { nombre: 'Certificación atecedentes judiciales RL.', url: '/documentos/2021/4.-Certificado-de-Antecedentes-RL-.pdf' },
-      { nombre: 'Nombre de los cargos directivos y de control.', url: '/documentos/2021/5.-Cargos-Directivos-y-de-Control.pdf' },
-      { nombre: 'Acta de asamblea.', url: '/documentos/2021/6.-Acta-Asamblea-General.pdf' },
-      { nombre: 'Identificación de fundadores.', url: '/documentos/2021/8.-Identificacion-Fundadores.pdf' },
-      { nombre: 'Pagos salariales.', url: '/documentos/2021/9.-Monto-Pago-Salariales.pdf' },
-      { nombre: 'Estados Financieros 2021.', url: '/documentos/2021/10.-Estados-Financieros-2021.pdf' }
-    ],
-    2022: [
-      { nombre: 'Denominacion de  domicilio.', url: '/Documentos/2022/1.-Denominacion-Identificacion.pdf' },
-      { nombre: 'Descripcion actividad meritoria.', url: '/documentos/2022/2.-Descripcion-Atividad-Meritoria.pdf' },
-      { nombre: 'Certificado requisitos RL.', url: '/documentos/2022/3.-Certificado-Requisitos-RL-1.pdf' },
-      { nombre: 'Certificación atecedentes judiciales RL.', url: '/documentos/2022/4.-Certificado-Antecentes.pdf' },
-      { nombre: 'Nombre de los cargos directivos y de control.', url: '/documentos/2022/5.-Cargos-Directivos-y-de-Control-1.pdf' },
-      { nombre: 'Acta de asamblea.', url: '/documentos/2022/6.-Acta-Asamblea-General-1.pdf' },
-      { nombre: 'Identificación de fundadores.', url: '/documentos/2022/8.-Identificacion-Fundadores-1.pdf' },
-      { nombre: 'Estados Financieros 2022.', url: '/documentos/2022/Estados-Financieros-2022.pdf' }
-    ],
-    2023: [
-      { nombre: 'Denominacion de domicilio.', url: '/documentos/2023/1.-Denominacion-Identificacion-Domicilio-pdf.pdf' },
-      { nombre: 'Descripcion actividad meritoria.', url: '/documentos/2023/2.-DescripcionActividad-Meitoria-pdf.pdf' },
-      { nombre: 'Certificado requisitos RL.', url: '/documentos/2023/3.-Certificado-Requisitos-RL-pdf.pdf' },
-      { nombre: 'Certificación antecedentes judiciales RL.', url: '/documentos/2023/4-Certificado-Antecedentes-RL.pdf' },
-      { nombre: 'Nombre de los cargos directivos y de control.', url: '/documentos/2023/5.-Cargos-Directivos-y-de-Control-pdf.pdf' },
-      { nombre: 'Acta de asamblea.', url: '/documentos/2023/6.-Acta-Asamblea-General-pdf.pdf' },
-      { nombre: 'Informe anual de gestión.', url: '/documentos/2023/7.-Informe-Anual-de-Gestion-pdf.pdf' },
-      { nombre: 'Identificación de fundadores.', url: '/documentos/2023/8.-Identificacion-Fundadores-pdf.pdf' },
-      { nombre: 'Estado de resultados 2023.', url: '/documentos/2023/Estado-de-Resultados-2023-pdf.pdf' },
-      { nombre: 'Formato 5245.', url: '/documentos/2023/FORMATO-5245.pdf' }
-    ]
-  };
+  2020: [
+    { nombre: 'Denominacion de  domicilio.', url: '/Documentos/2020/1.-Denominacion-domicilio.pdf' },
+    { nombre: 'Descripcion actividad meritoria.', url: '/Documentos/2020/2.-Descripcion-Actividad-Meritoria.pdf' },
+    { nombre: 'Certificado requisitos RL.', url: '/Documentos/2020/3.Certificado-Requisitos-RL.pdf' },
+    { nombre: 'Certificación antecedentes judiciales RL.', url: '/Documentos/2020/4.-Certificacion-Antecedentes-Judic-RL.pdf' },
+    { nombre: 'Nombre de los cargos cirectivos y de control.', url: '/Documentos/2020/5.-Nombre-de-los-Cargos-Directivos-y-de-Control.pdf' },
+    { nombre: 'Identificación de fundadores.', url: '/Documentos/2020/6.Identificacion-Fundadores.pdf' },
+    { nombre: 'Estados rinancieros 2020.', url: '/Documentos/2020/Estados-Financieros-2020.pdf' }
+  ],
+  2021: [
+    { nombre: 'Denominacion de  domicilio.', url: '/Documentos/2021/1.Denominacion-Domicilio-e-Identificacion.pdf' },
+    { nombre: 'Descripcion actividad meritoria.', url: '/Documentos/2021/2.-Descripcion-de-la-Actividad-Meritoria.pdf' },
+    { nombre: 'Certificado requisitos RL.', url: '/Documentos/2021/3.-Certificado-Requisitos-RL.pdf' },
+    { nombre: 'Certificación atecedentes judiciales RL.', url: '/Documentos/2021/4.-Certificado-de-Antecedentes-RL-.pdf' },
+    { nombre: 'Nombre de los cargos directivos y de control.', url: '/Documentos/2021/5.-Cargos-Directivos-y-de-Control.pdf' },
+    { nombre: 'Acta de asamblea.', url: '/Documentos/2021/6.-Acta-Asamblea-General.pdf' },
+    { nombre: 'Identificación de fundadores.', url: '/Documentos/2021/8.-Identificacion-Fundadores.pdf' },
+    { nombre: 'Pagos salariales.', url: '/Documentos/2021/9.-Monto-Pago-Salariales.pdf' },
+    { nombre: 'Estados Financieros 2021.', url: '/Documentos/2021/10.-Estados-Financieros-2021.pdf' }
+  ],
+  2022: [
+    { nombre: 'Denominacion de  domicilio.', url: '/Documentos/2022/1.-Denominacion-Identificacion.pdf' },
+    { nombre: 'Descripcion actividad meritoria.', url: '/Documentos/2022/2.-Descripcion-Atividad-Meritoria.pdf' },
+    { nombre: 'Certificado requisitos RL.', url: '/Documentos/2022/3.-Certificado-Requisitos-RL-1.pdf' },
+    { nombre: 'Certificación atecedentes judiciales RL.', url: '/Documentos/2022/4.-Certificado-Antecentes.pdf' },
+    { nombre: 'Nombre de los cargos directivos y de control.', url: '/Documentos/2022/5.-Cargos-Directivos-y-de-Control-1.pdf' },
+    { nombre: 'Acta de asamblea.', url: '/Documentos/2022/6.-Acta-Asamblea-General-1.pdf' },
+    { nombre: 'Identificación de fundadores.', url: '/Documentos/2022/8.-Identificacion-Fundadores-1.pdf' },
+    { nombre: 'Estados Financieros 2022.', url: '/Documentos/2022/Estados-Financieros-2022.pdf' }
+  ],
+  2023: [
+    { nombre: 'Denominacion de domicilio.', url: '/Documentos/2023/1.-Denominacion-Identificacion-Domicilio-pdf.pdf' },
+    { nombre: 'Descripcion actividad meritoria.', url: '/Documentos/2023/2.-DescripcionActividad-Meitoria-pdf.pdf' },
+    { nombre: 'Certificado requisitos RL.', url: '/Documentos/2023/3.-Certificado-Requisitos-RL-pdf.pdf' },
+    { nombre: 'Certificación antecedentes judiciales RL.', url: '/Documentos/2023/4-Certificado-Antecedentes-RL.pdf' },
+    { nombre: 'Nombre de los cargos directivos y de control.', url: '/Documentos/2023/5.-Cargos-Directivos-y-de-Control-pdf.pdf' },
+    { nombre: 'Acta de asamblea.', url: '/Documentos/2023/6.-Acta-Asamblea-General-pdf.pdf' },
+    { nombre: 'Informe anual de gestión.', url: '/Documentos/2023/7.-Informe-Anual-de-Gestion-pdf.pdf' },
+    { nombre: 'Identificación de fundadores.', url: '/Documentos/2023/8.-Identificacion-Fundadores-pdf.pdf' },
+    { nombre: 'Estado de resultados 2023.', url: '/Documentos/2023/Estado-de-Resultados-2023-pdf.pdf' },
+    { nombre: 'Formato 5245.', url: '/Documentos/2023/FORMATO-5245.pdf' }
+  ]
+};
+
 
   const toggleAno = (ano) => {
     setActivo((prev) => (prev === ano ? null : ano));
