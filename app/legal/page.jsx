7c991069e604b3f2f5fa44d769ee3498b6153d0f
natalia4566 <140755966+natalia4566@users.noreply.github.com>
2026-07-05 19:10:55 -0500

    actualización

--- app/legal/page.jsx
+++ app/legal/page.jsx
@@ -7,63 +7,6 @@ const Legal = () => {
   const [activo, setActivo] = useState(null);
 
   const documentosPorAno = {
-  2020: [
-    { nombre: 'Denominacion de  domicilio.', url: '/Documentos/2020/1.-Denominacion-domicilio.pdf' },
-    { nombre: 'Descripcion actividad meritoria.', url: '/Documentos/2020/2.-Descripcion-Actividad-Meritoria.pdf' },
-    { nombre: 'Certificado requisitos RL.', url: '/Documentos/2020/3.Certificado-Requisitos-RL.pdf' },
-    { nombre: 'Certificación antecedentes judiciales RL.', url: '/Documentos/2020/4.-Certificacion-Antecedentes-Judic-RL.pdf' },
-    { nombre: 'Nombre de los cargos cirectivos y de control.', url: '/Documentos/2020/5.-Nombre-de-los-Cargos-Directivos-y-de-Control.pdf' },
-    { nombre: 'Identificación de fundadores.', url: '/Documentos/2020/6.Identificacion-Fundadores.pdf' },
-    { nombre: 'Estados rinancieros 2020.', url: '/Documentos/2020/Estados-Financieros-2020.pdf' }
-  ],
-  2021: [
-    { nombre: 'Denominacion de  domicilio.', url: '/Documentos/2021/1.Denominacion-Domicilio-e-Identificacion.pdf' },
-    { nombre: 'Descripcion actividad meritoria.', url: '/Documentos/2021/2.-Descripcion-de-la-Actividad-Meritoria.pdf' },
-    { nombre: 'Certificado requisitos RL.', url: '/Documentos/2021/3.-Certificado-Requisitos-RL.pdf' },
-    { nombre: 'Certificación atecedentes judiciales RL.', url: '/Documentos/2021/4.-Certificado-de-Antecedentes-RL-.pdf' },
-    { nombre: 'Nombre de los cargos directivos y de control.', url: '/Documentos/2021/5.-Cargos-Directivos-y-de-Control.pdf' },
-    { nombre: 'Acta de asamblea.', url: '/Documentos/2021/6.-Acta-Asamblea-General.pdf' },
-    { nombre: 'Identificación de fundadores.', url: '/Documentos/2021/8.-Identificacion-Fundadores.pdf' },
-    { nombre: 'Pagos salariales.', url: '/Documentos/2021/9.-Monto-Pago-Salariales.pdf' },
-    { nombre: 'Estados Financieros 2021.', url: '/Documentos/2021/10.-Estados-Financieros-2021.pdf' }
-  ],
-  2022: [
-    { nombre: 'Denominacion de  domicilio.', url: '/Documentos/2022/1.-Denominacion-Identificacion.pdf' },
-    { nombre: 'Descripcion actividad meritoria.', url: '/Documentos/2022/2.-Descripcion-Atividad-Meritoria.pdf' },
-    { nombre: 'Certificado requisitos RL.', url: '/Documentos/2022/3.-Certificado-Requisitos-RL-1.pdf' },
-    { nombre: 'Certificación atecedentes judiciales RL.', url: '/Documentos/2022/4.-Certificado-Antecentes.pdf' },
-    { nombre: 'Nombre de los cargos directivos y de control.', url: '/Documentos/2022/5.-Cargos-Directivos-y-de-Control-1.pdf' },
-    { nombre: 'Acta de asamblea.', url: '/Documentos/2022/6.-Acta-Asamblea-General-1.pdf' },
-    { nombre: 'Identificación de fundadores.', url: '/Documentos/2022/8.-Identificacion-Fundadores-1.pdf' },
-    { nombre: 'Estados Financieros 2022.', url: '/Documentos/2022/Estados-Financieros-2022.pdf' }
-  ],
-  2023: [
-    { nombre: 'Denominacion de domicilio.', url: '/Documentos/2023/1.-Denomincacion-Identificacion-Domicilio-pdf.pdf' },
-    { nombre: 'Descripcion actividad meritoria.', url: '/Documentos/2023/2.-DescripcionlActividad-Meitoria-pdf.pdf' },
-    { nombre: 'Certificado requisitos RL.', url: '/Documentos/2023/3.-Certfificado-Requisitos-R.L-pdf.pdf' },
-    { nombre: 'Certificación antecedentes judiciales RL.', url: '/Documentos/2023/4-Certificado-Antecedentes-RL.pdf' },
-    { nombre: 'Nombre de los cargos directivos y de control.', url: '/Documentos/2023/5.-Cargos-Directivos-y-de-Control-pdf.pdf' },
-    { nombre: 'Acta de asamblea.', url: '/Documentos/2023/6.-Acta-Asamblea-General-pdf.pdf' },
-    { nombre: 'Informe anual de gestión.', url: '/Documentos/2023/7.-Informe-Anual-de-Gestion-pdf.pdf' },
-    { nombre: 'Identificación de fundadores.', url: '/Documentos/2023/8.-Identificacion-Fundadores-pdf.pdf' },
-    { nombre: 'Estado de resultados 2023.', url: '/Documentos/2023/Estado-de-Resultados-2023-pdf.pdf' },
-    { nombre: 'Formato 5245.', url: '/Documentos/2023/FORMATO-5245.pdf' }
-  ],
-  2024: [
-  { nombre: 'Denominacion  Identificacion Domicilio', url: '/Documentos/2024/1. Denominacion  Identificacion Domicilio.pdf' },
-  { nombre: 'Descripcion Actividad Meritoria', url: '/Documentos/2024/2. Descripcion Actividad Meritoria.pdf' },
-  { nombre: 'Certificado Requisitos R.L.', url: '/Documentos/2024/3. Certificado Requisitos R.L..pdf' },
-  { nombre: 'Certificado Antecedentes R.L.', url: '/Documentos/2024/4. Certificado Antecedentes R.L..pdf' },
-  { nombre: 'Cargos Directivos y de Control', url: '/Documentos/2024/5. Cargos Directivos y de Control.pdf' },
-  { nombre: 'Acta Asamblea  2025 EF ', url: '/Documentos/2024/6. Acta Asamblea  2025 EF 2024.pdf' },
-  { nombre: 'Informe Anual Gestion ', url: '/Documentos/2024/7. Infomre Anual Gestion 2024.pdf' },
-  { nombre: 'Identificacion Fundadores', url: '/Documentos/2024/8. Identificacion Fundadores.pdf' },
-  { nombre: 'Monto total Donaciones ', url: '/Documentos/2024/9. Monto total Donaciones 2024.pdf' },
-  { nombre: 'Monto total de pagos Salariales.', url: '/Documentos/2024/10. Monto total Pagos Salariales.pdf' },
-  { nombre: 'Estado de resultados 2024', url: '/Documentos/2024/ESTADO DE RESULTADOS 2024.pdf' },
-  { nombre: 'Formato 5245.', url: '/Documentos/2024/Formato 5245.pdf' }
-  
-  ],
   2025: [
   { nombre: 'Denominacion Identificacion Domicilio', url: '/Documentos/2025/1. Denominacion Identificacion Domicilio.pdf' },
   { nombre: 'Descripcion Actividad Meritoria', url: '/Documentos/2025/2. Descripcion Actividad Meritoria (1).pdf' },
@@ -96,7 +39,7 @@ const Legal = () => {
           <p>La Fundación Tu Amparo y Tu Fortaleza ofrece aquí la siguiente información en cumplimiento del artículo 364-5 del Estatuto Tributario.</p>
         </div>
       </div>
-      <h3 className={styles.subtitulo}>Aquí nuestra normatividad clasificada por año</h3>
+      <h3 className={styles.subtitulo}>En esta sección se encuentra disponible la información actualizada del registro web de la Fundación Tu Amparo y Tu Fortaleza</h3>
       {Object.entries(documentosPorAno).map(([ano, docs]) => (
         <div key={ano} className={styles.bloque}>
           <button className={styles.boton} onClick={() => toggleAno(ano)}>
